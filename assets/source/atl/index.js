296f3f33f063df8eec84c6a136c86cea53116990
Sergey M <littlekpa6i@gmail.com>
2023-06-21 00:41:17 +0200

    updated styles

--- assets/source/atl/index.js
+++ assets/source/atl/index.js
@@ -3,5 +3,8 @@ import $ from 'jquery'
 
 $(document).on('click', '.annex-sidebar-link', function (e) {
     console.log(e.target);
-    // window.parent.location = document.referrer;
+    e.preventDefault();
+    const href = $(this).attr('href');
+    console.log(href);
+    window.parent.location = href;
 })
