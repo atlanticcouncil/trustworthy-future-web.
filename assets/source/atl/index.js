4319f6758e9e429361a2d05d5af5dbd82605b982
Sergey M <littlekpa6i@gmail.com>
2023-06-21 00:56:55 +0200

    updated styles

--- assets/source/atl/index.js
+++ assets/source/atl/index.js
@@ -1,10 +1,8 @@
 import './style.scss'
 import $ from 'jquery'
 
-$(document).on('click', '.annex-sidebar-link', function (e) {
-    console.log(e.target);
+$(document).on('click', '.iframe-link', function (e) {
     e.preventDefault();
     const href = $(this).attr('href');
-    console.log(href);
     window.parent.location = href;
 })
