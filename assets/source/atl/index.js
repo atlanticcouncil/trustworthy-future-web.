88d980589f60bba26f37a1eaf7ee03e8ae339701
Sergey M <littlekpa6i@gmail.com>
2023-09-14 13:48:31 +0200

    updated styles

--- assets/source/atl/index.js
+++ assets/source/atl/index.js
@@ -6,9 +6,10 @@ $(document).on('click', '.iframe-link', function (e) {
     window.parent.location = $(this).attr('href');
 })
 
-$(document).on('click', '.mobile-menu-top-level-toggler', function(e) {
+$(document).on('click', '.menu_checkbox_label', function(e) {
     e.preventDefault();
 
+    $(this).toggleClass('active');
     $('.mobile-menu-top-level').toggleClass('open');
     $('.mobile-menu-expanded-level').toggleClass('open');
 })
